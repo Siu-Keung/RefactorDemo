364b06002b10a33e316ba4087a7dc58e0e77c004
siu_keung@outlook.com <19950811a>
2018-08-26 13:33:28 +0800

    [Dylan] extracted the switch/case block in the Receipt.getReceiptStr method into an individual method named "getItemSubTotalPrice";

---
+++ src/main/java/rentalstore/Receipt.java
@@ -0,0 +1,89 @@
+package rentalstore;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Receipt {
+    private Customer customer;
+    private List<Rental> rentals;
+
+    public Receipt(){
+
+    }
+
+    public Receipt(Customer customer, List<Rental> rentals) {
+        this.customer = customer;
+        this.rentals = rentals;
+    }
+
+    private double getItemSubTotalPrice(Rental rental){
+        double subTotalPrice = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                subTotalPrice += 2;
+                if (rental.getDayRented() > 2) {
+                    subTotalPrice += (rental.getDayRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                subTotalPrice += rental.getDayRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                subTotalPrice += 1.5;
+                if (rental.getDayRented() > 3) {
+                    subTotalPrice += (rental.getDayRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return subTotalPrice;
+    }
+
+    public String getHeaderStr(){
+        return "<H1>Rentals for <EM>" + this.customer.getName() + "</EM></H1><P>";
+    }
+
+    public String getReceiptStr(){
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + this.customer.getName() + "\n";
+        List<Rental> rentals = this.rentals.stream().distinct().collect(Collectors.toList());
+        Iterator<Rental> rentalSetIterator = rentals.iterator();
+        while (rentalSetIterator.hasNext()) {
+            double subTotalPrice = 0;
+            Rental each = rentalSetIterator.next();
+            subTotalPrice = getItemSubTotalPrice(each);
+            //add frequent renter points
+            frequentRenterPoints++;
+            //add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(subTotalPrice) + "\n";
+            totalAmount += subTotalPrice;
+        }
+
+        //add footer lines
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Rental> getRentals() {
+        return rentals;
+    }
+
+    public void setRentals(List<Rental> rentals) {
+        this.rentals = rentals;
+    }
+}
---
+++ src/test/java/rentalstore/ReceiptTest.java
@@ -0,0 +1,63 @@
+package rentalstore;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ReceiptTest {
+
+    @Test
+    public void should_output_receipt_with_total_price_2_when_rent_REGULAR_movie_for_one_day() {
+        Customer customer = new Customer("Dylan");
+        List<Rental> rentals = Arrays.asList(
+                new Rental(new Movie("Titanic", Movie.REGULAR), 1)
+        );
+        Receipt receipt = new Receipt(customer, rentals);
+        String expectedReceipt =
+                "Rental Record for Dylan\n" +
+                        "\tTitanic\t2.0\n" +
+                        "Amount owed is 2.0\n" +
+                        "You earned 1 frequent renter points";
+        assertThat(receipt.getReceiptStr(), is(expectedReceipt));
+    }
+
+    @Test
+    public void should_output_receipt_with_total_price_6_point_5_when_rent_NEW_RELEASE_movie_1_day_and_REGULAR_for_3_days() {
+        Customer customer = new Customer("Dylan");
+        List<Rental> rentals = Arrays.asList(
+                new Rental(new Movie("Titanic", Movie.REGULAR), 3),
+                new Rental(new Movie("The Ring", Movie.NEW_RELEASE), 1)
+        );
+        Receipt receipt = new Receipt(customer, rentals);
+        String expectedReceipt =
+                "Rental Record for Dylan\n" +
+                        "\tTitanic\t3.5\n" +
+                        "\tThe Ring\t3.0\n" +
+                        "Amount owed is 6.5\n" +
+                        "You earned 2 frequent renter points";
+        assertThat(receipt.getReceiptStr(), is(expectedReceipt));
+    }
+
+    @Test
+    public void should_return_receipt_with_total_price_8_when_rent_REGULAR_3_days_and_NEW_RELEASE_1_day_and_CHILDRENS_3_days() {
+        Customer customer = new Customer("Dylan");
+        List<Rental> rentals = Arrays.asList(
+                new Rental(new Movie("Titanic", Movie.REGULAR), 3),
+                new Rental(new Movie("The Ring", Movie.NEW_RELEASE), 1),
+                new Rental(new Movie("One Day", Movie.CHILDREN), 3)
+        );
+        Receipt receipt = new Receipt(customer, rentals);
+        String expectedReceipt =
+                "Rental Record for Dylan\n" +
+                        "\tTitanic\t3.5\n" +
+                        "\tThe Ring\t3.0\n" +
+                        "\tOne Day\t1.5\n" +
+                        "Amount owed is 8.0\n" +
+                        "You earned 3 frequent renter points";
+        assertThat(receipt.getReceiptStr(), is(expectedReceipt));
+    }
+
+
+}
