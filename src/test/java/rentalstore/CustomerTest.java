3149ba6e4bb201047edd6e33443d1df9b3291510
siu_keung@outlook.com <19950811a>
2018-08-26 12:32:06 +0800

    [Dylan] added a unit test to check whether the total price is 6.5 in premise of renting one REGULAR movie for three days and one NEW_RELEASE movie for one day

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -13,14 +13,28 @@ import static org.junit.Assert.*;
 public class CustomerTest {
 
     @Test
-    public void should_output_receipt_with_total_price_3() {
+    public void should_output_receipt_with_total_price_2_when_rent_one_REGULAR_movie_for_one_day() {
         Customer customer = new Customer("Dylan");
-        customer.addRental(new Rental(new Movie("Titanic", Movie.NEW_RELEASE), 1));
+        customer.addRental(new Rental(new Movie("Titanic", Movie.REGULAR), 1));
         String expectedReceipt =
                 "Rental Record for Dylan\n" +
-                        "\tTitanic\t3.0\n" +
-                        "Amount owed is 3.0\n" +
+                        "\tTitanic\t2.0\n" +
+                        "Amount owed is 2.0\n" +
                         "You earned 1 frequent renter points";
+        assertThat(customer.statement(), is(expectedReceipt));
+    }
+
+    @Test
+    public void should_output_receipt_with_total_price_6_point_5_when_rent_1_NEW_RELEASE_movie_1_day_and_1_REGULAR_for_3_days() {
+        Customer customer = new Customer("Dylan");
+        customer.addRental(new Rental(new Movie("Titanic", Movie.REGULAR), 3));
+        customer.addRental(new Rental(new Movie("The Ring", Movie.NEW_RELEASE), 1));
+        String expectedReceipt =
+                "Rental Record for Dylan\n" +
+                        "\tTitanic\t3.5\n" +
+                        "\tThe Ring\t3.0\n" +
+                        "Amount owed is 6.5\n" +
+                        "You earned 2 frequent renter points";
         assertThat(customer.statement(), is(expectedReceipt));
     }
 
